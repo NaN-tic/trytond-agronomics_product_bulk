5e1bdc237623f89255175ff04d84a8070025d704
Jared Esparza <jared.esparza@nan-tic.com>
2022-06-20 09:52:04 +0200

    Replace test setuptools command by unittest discover

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,20 +1,15 @@
 # This file is part agronomics_product_bulk module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-import unittest
 
 
 from trytond.tests.test_tryton import ModuleTestCase
-from trytond.tests.test_tryton import suite as test_suite
+from trytond.modules.company.tests import CompanyTestMixin
 
 
-class AgronomicsProductBulkTestCase(ModuleTestCase):
+class AgronomicsProductBulkTestCase(CompanyTestMixin, ModuleTestCase):
     'Test Agronomics Product Bulk module'
     module = 'agronomics_product_bulk'
 
 
-def suite():
-    suite = test_suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            AgronomicsProductBulkTestCase))
-    return suite
+del ModuleTestCase
